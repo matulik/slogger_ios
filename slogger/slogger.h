3bbef60cb55eebe2fd378e0a541434a76045ff0c
Kamil Matuła <matulik@o2.pl>
2016-07-12 09:18:21 +0200

    Initial commit

---
+++ slogger/slogger.h
@@ -0,0 +1,19 @@
+//
+//  slogger.h
+//  slogger
+//
+//  Created by Kamil Matula on 12/07/16.
+//  Copyright © 2016 Kamil Matula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for slogger.
+FOUNDATION_EXPORT double sloggerVersionNumber;
+
+//! Project version string for slogger.
+FOUNDATION_EXPORT const unsigned char sloggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <slogger/PublicHeader.h>
+
+
